1ae1b0dcadbd3a0e0803dfad85819b39762e8413
obe cornsnook <obe.cornsnook@gmail.com>
2015-04-13 20:12:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LochStockSpock::Application.config.secret_token = '513dbec40907681189061f6b3469d1bc975d647ab944d201973af8129939fa4992438bfb176ee5d3ffdb4470ea2ad54042998f02442cdf4bff5e3c72954701ec'
